ef4269215e18bfda2773c54620de4c1b33a0cfd3
lemajr <erickbale360@gmail.com>
2025-04-15 00:12:23 +0300

    permission granted

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi.responses import HTMLResponse
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database.core import engine, Base
 from .entities.todo import Todo  # Import models to register them
 from .entities.user import User  # Import models to register them
@@ -11,6 +12,13 @@ configure_logging(LogLevels.info)
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 Base.metadata.create_all(bind=engine)
 
 register_routes(app)
